64ecf82ea7040ebd6f079c1e127c0737318c7661
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-02-28 23:41:15 +0800

    0.0.1

---
+++ search_index.js
@@ -0,0 +1,4 @@
+var searchModuleIndex = ["vaker",];
+var searchIndex = ["DataFaker","fake_data","fake_data_wdf",];
+var searchModuleData = [["","vaker.html"],];
+var searchData = [["vaker","","vaker.html#DataFaker","struct "],["vaker","","vaker.html#fake_data","fn "],["vaker","","vaker.html#fake_data_wdf","fn "],];
